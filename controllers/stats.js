300b4ba9a6e780f4ecceaa08092f0af2e22ae869
prawsang <prawsang.c@gmail.com>
2021-05-18 08:00:30 +0700

    add by score stats

--- controllers/stats.js
+++ controllers/stats.js
@@ -1,13 +1,32 @@
 const Sequelize = require('sequelize')
+const db = require('../config/db')
 const Application = require('../models/Application')
 const Result = require('../models/Result')
-const { paginate } = require('../utils/pagination')
+const { errorResponse } = require('../utils/error')
 
 const byScore = async (req, res) => {
-	const { category, page, limit } = req.query
-	Application.findAll({})
-		.then((result) => res.send(result))
-		.catch((err) => console.log(err))
+	const { category, order } = req.query
+	try {
+		const results = await db.query(
+			`SELECT * from apps INNER
+		JOIN (
+			SELECT DISTINCT ON ("applicationId") results."vulpixScore", "applicationId" 
+			FROM results ORDER BY "applicationId" ASC, "createdAt" DESC
+		) temp ON apps."identifier" = temp."applicationId" 
+		${category ? `WHERE apps."categorySlug" = $categorySlug` : ''} 
+		ORDER BY "vulpixScore" ${order === 'asc' ? 'ASC' : 'DESC'}
+		LIMIT 100`,
+			{
+				bind: {
+					categorySlug: category,
+				},
+				type: Sequelize.QueryTypes.SELECT,
+			},
+		)
+		res.send(results)
+	} catch (err) {
+		res.status(500).json(errorResponse(err))
+	}
 }
 
 module.exports = {
