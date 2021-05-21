9945bb9ef90460433543b01502e6831c487dd749
prawsang <prawsang.c@gmail.com>
2021-05-21 11:47:07 +0700

    cors

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 
-// const Result = require('./models/Result')
-
 const envFile = process.env.NODE_ENV ? `${process.env.NODE_ENV}.env` : '.env'
 dotenv.config({ path: envFile })
 
@@ -14,8 +12,22 @@ db.authenticate()
 
 // db.sync()
 
+const whitelist = [
+	'https://vulpix2.herokuapp.com/',
+	'http://vulpix2.herokuapp.com/',
+]
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	},
+}
+
 const app = express()
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api', require('./routes'))
