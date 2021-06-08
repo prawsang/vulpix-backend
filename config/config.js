a3ada04bdf16cc819d4ac29328944431397e5f7e
prawsang <prawsang.c@gmail.com>
2021-06-08 20:46:59 +0700

    edit database connection

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
 	development: {
 		username: 'postgres',
-		password: 'postgres',
+		password: process.env.DB_PASSWORD,
 		database: 'vulpix_dev',
 		host: process.env.DB_HOST,
 		dialect: 'postgres',
 	},
 	test: {
 		username: 'postgres',
-		password: 'postgres',
+		password: process.env.DB_PASSWORD,
 		database: 'vulpix_test',
 		host: process.env.DB_HOST,
 		dialect: 'postgres',
 	},
 	production: {
 		username: 'postgres',
-		password: 'postgres',
+		password: process.env.DB_PASSWORD,
 		database: 'vulpix_prod',
 		host: process.env.DB_HOST,
 		dialect: 'postgres',
--- config/db.js
+++ config/db.js
@@ -1,17 +1,21 @@
 const Sequelize = require('sequelize')
-module.exports = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
-	host: process.env.DB_HOST,
-	dialect: 'postgres',
-	// dialectOptions: {
-	// 	ssl: {
-	// 		require: true,
-	// 		rejectUnauthorized: false, // <<<<<< YOU NEED THIS
-	// 	},
-	// },
-	pool: {
-		max: 5,
-		min: 0,
-		acquire: 30000,
-		idle: 10000,
+//postgres:vulpix@localhost:5432/vulpix?sslmode=require
+postgres: module.exports = new Sequelize(
+	`postgres://postgres:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+	{
+		host: process.env.DB_HOST,
+		dialect: 'postgres',
+		// dialectOptions: {
+		// 	ssl: {
+		// 		require: true,
+		// 		rejectUnauthorized: false, // <<<<<< YOU NEED THIS
+		// 	},
+		// },
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000,
+		},
 	},
-})
+)
